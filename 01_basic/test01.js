1d9e82424397970e5b16b9ea8232da77a6679b74
sutapagit <163644858+sutapagit@users.noreply.github.com>
2026-03-09 11:10:27 +0000

    changed code

--- 01_basic/test01.js
+++ 01_basic/test01.js
@@ -1,1 +1,11 @@
 console.log('Hello, World!');
+let newDate = new Date();
+// console.log(newDate.toDateString());
+// console.log(newDate.getDate());
+// console.log(newDate.getMonth());
+// console.log(newDate.getFullYear());
+
+// console.log(newDate.toLocaleString());
+
+console.log(newDate());
+
