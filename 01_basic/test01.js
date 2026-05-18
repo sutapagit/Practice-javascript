35ff57c0278cb8ab8200293e59702b474ea7958d
Sutapagit <sutapa14033@yahoo.in>
2026-05-18 08:56:35 +0000

    initial commit

--- 01_basic/test01.js
+++ 01_basic/test01.js
@@ -1,7 +1,7 @@
 console.log('Hello, World!');
 let newDate = new Date();
 console.log(newDate.toDateString());
-// console.log(newDate.getDate());
+ console.log(newDate.getDate());
 // console.log(newDate.getMonth());
 // console.log(newDate.getFullYear());
 
